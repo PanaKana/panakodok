607d89cc2656f87db9bdb51e867c68e10283e3eb
Ronny Pratama <ronnyprtm@gmail.com>
2019-07-29 02:24:00 +0800

    newbot

--- bot.js
+++ bot.js
@@ -1,11 +1,9 @@
-// require the discord.js module
 const Discord = require('discord.js');
 
-// create a new Discord client
 const client = new Discord.Client();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+ 
+
 client.on('ready', () => {
 
     console.log('I am ready!');
@@ -23,5 +21,7 @@ client.on('message', message => {
        }
 
 });
+
+ 
 // login to Discord with your app's token
 client.login('NTUwMTUxMjM0OTE3NjI5OTUz.XT2SPQ.PPZvee7smlrKp2vzsj4NOAc9s7o');
