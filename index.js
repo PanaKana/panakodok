ad34ce5ee3a12885cc9f051e29f48540dee067e7
Ronny Pratama <ronnyprtm@gmail.com>
2019-07-29 02:08:40 +0800

    Changes index.js

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ const client = new Discord.Client();
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
-	// console.log('Ready!');
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
 // login to Discord with your app's token
 client.login('NTUwMTUxMjM0OTE3NjI5OTUz.XT2SPQ.PPZvee7smlrKp2vzsj4NOAc9s7o');
